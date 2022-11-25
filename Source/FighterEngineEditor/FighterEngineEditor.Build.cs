96c8c60e9ddb666fe7e73b5286f78c23cedeb38c
WistfulHopes <kp72002@gmail.com>
2022-11-25 15:37:18 -0800

    no more bp errors

---
+++ Source/FighterEngineEditor/FighterEngineEditor.Build.cs
@@ -0,0 +1,30 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class FighterEngine : ModuleRules
+{
+	public FighterEngine(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		if (Target.bBuildEditor)
+		{
+			OptimizeCode = CodeOptimization.Never;
+		}
+		
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"FighterEngine",
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] {  });
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+		
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
